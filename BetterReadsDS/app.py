7c889d0d2af5ff9da5fead0742492c7c1e43fdad
Kumar Veeravel <mvkumar14@gmail.com>
2020-03-11 10:56:24 -0700

    Update endpoint to use POST request with 2 parameters

--- BetterReadsDS/app.py
+++ BetterReadsDS/app.py
@@ -27,26 +27,67 @@ def create_app():
     # to api documentation
     def root():
         return render_template('base.html',page_name='home')
-    @app.route('/search/<search_term>')
 
-    # the input is going to be a string
-    # output will be a list of books from the google api
-    # formatting of the output should be in OUT_LIST format
-    def search(search_term):
+    @app.route('/test',methods=['POST'])
+    def test():
+        print('test')
+        test_val = request.get_json(force=True)
+        print(test_val)
+        return "3"
+
+    @app.route('/search', methods=['GET','POST'])
+    def search():
+        # variables used throughout the function
         GOOGLE_KEY = os.environ['GOOGLE_KEY']
-        response = requests.get('https://www.googleapis.com/books/v1/volumes?q='
-            + search_term
-            + '&key='
-            + GOOGLE_KEY)
-        result = json.loads(response.text)
-
         relevant_details=['id','title','authors','publisher',
                   'publishedDate','description','industryIdentifiers',
                   'pageCount','categories','thumbnail','smallThumbnail',
                   'language','webReaderLink','textSnippet','isEbook']
 
-        output = process_list(result['items'],relevant_details)
-        return jsonify(output)
+        # Retreive the information from the POST request body
+
+        # try:
+        #     input_data = request.get_json()
+        # except:
+        #     print("that didn't work")
+        # print(input_data)
+
+        input_data = request.get_json(force=True)
+
+        # Try to access keys from the post request
+        try:
+            if input_data['type'] == 'googleId':
+                search_id = input_data['query']
+                response = requests.get('https://www.googleapis.com/books/v1/volumes/'
+                                        + search_id
+                                        + '?key='
+                                        + GOOGLE_KEY)
+                # If invalid google id, then display/return an error message
+                result = json.loads(response.text)
+                output = process_list([result],relevant_details)
+                return jsonify(output)
+
+            elif input_data['type'] == 'search':
+                search_term = input_data['query']
+                response = requests.get('https://www.googleapis.com/books/v1/volumes?q='
+                    + search_term
+                    + '&key='
+                    + GOOGLE_KEY)
+                result = json.loads(response.text)
+                output = process_list(result['items'],relevant_details)
+                return jsonify(output)
+            else:
+                message = """ The value for the 'type' key was invalid.
+                 Please change the value to 'googleId', or 'search' """
+                return render_template('echo.html',page_name='error',
+                                        echo=message)
+
+        # If you can't access keys from the post request display error message
+        except KeyError:
+            message = """ The key wasn't in the request body"""
+            return render_template('echo.html',page_name='error', echo=message)
+
+        return render_template('echo.html',page_name='search')
 
     @app.route('/subject_list')
     # input will be subject heading (this should be a valid value)
