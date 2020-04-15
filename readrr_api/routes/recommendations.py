10c3a1e627b70612b6b3ca896940f364d91c0cbd
jose-marquez89 <jlmarquez89@gmail.com>
2020-04-15 06:04:02 -0500

    adds necessary tasks

--- readrr_api/routes/recommendations.py
+++ readrr_api/routes/recommendations.py
@@ -4,6 +4,7 @@ import json
 import requests
 from flask import Flask, request, jsonify
 
+# TODO: add a way to communicate with google books api
 # TODO: add pickled model for use within reccomendations route
 
 @recomendations.route('/recommendations/<int:userid>',methods=['GET'])
@@ -22,7 +23,16 @@ def recommend():
     user_data_url = (shelf_endpoint + userid)
     user_books = requests.get(user_data_url)
     user_books = json.loads(user_books.text)
-
+    
+    # TODO: take one random book where "favorite" == True
+    #       (or some other method), and use the book to 
+    #       provide recommendations. Include the book
+    #       title in the response so that web and iOS
+    #       can reflect this back to the user (transparency)
+    
+    # TODO: call google books API for data on recommendations
+    
+    # TODO: correct output and include book used for recommendation
     output = {'interest':'hardcoded_reccs',
               'recommendations':output_reccs}
     
