e633fa8f3853be3c997df00d10da3abda4d737ed
jose-marquez89 <jlmarquez89@gmail.com>
2020-04-16 10:08:32 -0500

    cleans some messy code

--- readrr_tools/gb_search.py
+++ readrr_tools/gb_search.py
@@ -6,36 +6,32 @@ import requests
 class GBWrapper:
     """
     Provides functions to search using all
-    available simple methods 
+    available simple methods
     """
     def __init__(self, method='intitle'):
         """API instance constructor
-        
+
         :reference: https://developers.google.com/books/docs/v1/using
         :param method: search method for api, default: 'intitle'
         """
         self.method = method
-    
+
     def search(self, terms):
         """Gets json response for search terms"""
         numeric_methods = ['isbn', 'lccn', 'oclc']
-        
+
         if self.method not in numeric_methods:
             tokens = terms.split()
             terms = '+'.join(tokens)
-            
-         
+
         base_url = 'https://www.googleapis.com/books/v1/volumes?q='
         res = requests.get(base_url + self.method + terms)
         try:
             res.raise_for_status()
-        
+
         except Exception as err:
             msg = "An exception occurred," \
                   "check search terms and try again?"
             print(msg)
-        
+
         return res.json()
-        
-        
-        
