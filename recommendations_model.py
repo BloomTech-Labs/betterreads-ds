201da454c5d3e08c4b90d6dc8e225e8b322c7cf9
Michael Rowland <mrowland13@gmail.com>
2020-05-04 21:52:21 -0700

    PEP8 Linting

--- recommendations_model.py
+++ recommendations_model.py
@@ -10,16 +10,21 @@ from surprise import accuracy
 from surprise.model_selection import cross_validate, train_test_split
 from surprise.model_selection import train_test_split
 
+
 def data_import():
-    books = pd.read_csv('https://raw.githubusercontent.com/zygmuntz/goodbooks-10k/master/books.csv')
-    ratings = pd.read_csv('https://raw.githubusercontent.com/zygmuntz/goodbooks-10k/master/ratings.csv')
+    books = pd.read_csv('https://raw.githubusercontent.com/zygmuntz/\
+        goodbooks-10k/master/books.csv')
+    ratings = pd.read_csv('https://raw.githubusercontent.com/zygmuntz/\
+        goodbooks-10k/master/ratings.csv')
     return books, ratings
 
+
 def clean_book_titles(title):
-  title = re.sub(r'\([^)]*\)', '', title)
-  title = re.sub(' + ', ' ', title)
-  title = title.strip()
-  return title
+    title = re.sub(r'\([^)]*\)', '', title)
+    title = re.sub(' + ', ' ', title)
+    title = title.strip()
+    return title
+
 
 # should input be set to None by default?
 def book_cleaning(books):
@@ -31,15 +36,18 @@ def book_cleaning(books):
     books['title'] = books['title'].apply(clean_book_titles)
     return books
 
+
 def create_matrix(books, ratings):
     books = books.copy()
     ratings = ratings.copy()
 
     df = pd.merge(ratings, books, on='book_id')
-    df  = df.drop_duplicates(['user_id', 'title'])
+    df = df.drop_duplicates(['user_id', 'title'])
 
-    matrix = df.pivot(index='title', columns='user_id', values='rating').fillna(0)
+    matrix = df.pivot(
+        index='title', columns='user_id', values='rating').fillna(0)
     return df
+
 
 def create_model(df):
     df = df.copy()
@@ -60,9 +68,9 @@ def create_model(df):
     cross_validate(algo, data, measures=['RMSE', 'MAE'], cv=5, verbose=True)
     return
 
+
 if __name__ == "__main__":
     books, ratings = data_import()
     books = book_cleaning(books)
     matrix = create_matrix(books, ratings)
     create_model(matrix)
-    
