d2cbd3704e349488ef2558c5cfe4bf757d6fe18d
Michael Rowland <mrowland13@gmail.com>
2020-05-07 10:38:13 -0700

    updated DB_NAME to enable  successful connection

--- Connection.py
+++ Connection.py
@@ -6,6 +6,7 @@ def connection():
     DB_USER = config('DB_USERNAME')
     DB_PASS = config('DB_PASSWORD')
     DB_HOST = config('DB_HOST')
-    DB_URL = f'postgres://{DB_USER}:{DB_PASS}@{DB_HOST}:5432/{DB_USER}'
+    DB_NAME = config('DB_NAME')
+    DB_URL = f'postgres://{DB_USER}:{DB_PASS}@{DB_HOST}:5432/{DB_NAME}'
     engine = create_engine(DB_URL)
     return engine
