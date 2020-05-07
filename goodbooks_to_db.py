6412f82cfd3941e8eae77374c5885ccb8aad6b60
Michael Rowland <mrowland13@gmail.com>
2020-05-07 10:59:09 -0700

    adds remaining .csv files from goodbooks repo to our database

---
+++ goodbooks_to_db.py
@@ -0,0 +1,21 @@
+import os
+
+import pandas as pd
+
+from connection import connection
+
+
+if __name__ == "__main__":
+
+    engine = connection()
+    path = 'goodbooks-10k/'
+
+    for file in os.listdir(path):
+        if file.endswith(".csv"):
+            df = pd.read_csv(path + file)
+            df.to_sql(
+                f'goodbooks_{file.split(".")[0]}',
+                con=engine,
+                if_exists='replace',
+                index=False
+                )
