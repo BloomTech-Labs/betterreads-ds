e9b4e815329904516023c9df142dd2f36e818837
jose-marquez89 <jlmarquez89@gmail.com>
2020-05-14 17:21:37 -0500

    removes crude book selection mechanism

--- readrr_api/routes/recommendations.py
+++ readrr_api/routes/recommendations.py
@@ -35,52 +35,10 @@ def recommend():
     Provide recommendations based on user bookshelf.
     """
     user_books = request.get_json()
-    
+
     for book in user_books:
         if book['favorite']:
             favorites.append(book['title'])
 
-    # to add some randomness
-    random.shuffle(favorites)
-
-    # if there are no favorites, recommend based on first book
-    if len(favorites) >= 1:
-        for i in range(len(favorites)):
-            try:
-                target_book = favorites[i]
-                neighbors = get_recommendations(target_book)
-                acquired = True
-                break
-            except ValueError:
-                continue
-
-    else:
-        neighbors, acquired, target_book = shelf_iterate(user_books)
-
-    # if book is not in known titles, recommend an alternative
-    if not acquired:
-        neighbors, acquired, target_book = shelf_iterate(user_books)
-        if neighbors is None:
-            target_book = "#GIRLBOSS"
-            neighbors = get_recommendations(target_book)
-    else:
-        neighbors = get_recommendations(target_book)
-
-    output_recs = []
-    logging.info("Neighbors:" + str(neighbors))
-
-    # skip first book
-    for book in neighbors[1:]:
-        book_data = api.search(book)
-        target_data = book_data['items'][0]
-        target_json = retrieve_details(target_data)
-        output_recs.append(target_json)
-
-    # change 'id' to 'googleId' at every list index
-    for item in output_recs:
-        item['googleId'] = item.pop('id')
-
-    output = {'based_on': target_book,
-              'recommendations': output_recs}
 
     return jsonify(output)
