290d1999fed62d96204ffdd0df1d09c58267c278
jose-marquez89 <jlmarquez89@gmail.com>
2020-05-15 06:39:13 -0500

    adds workaround for missing attribute in pickled tfidf

---
+++ gunicorn_entrypoint.py
@@ -0,0 +1,25 @@
+import gunicorn.app.base
+
+from readrr_api import create_app
+from readrr_api.route_tools.recommender import tokenize
+
+
+class StandaloneApplication(gunicorn.app.base.BaseApplication):
+
+    def __init__(self, app, options=None):
+        self.options = options or {}
+        self.application = app
+        super().__init__()
+
+    def load_config(self):
+        config = {key: value for key, value in self.options.items()
+                  if key in self.cfg.settings and value is not None}
+        for key, value in config.items():
+            self.cfg.set(key.lower(), value)
+
+    def load(self):
+        return self.application
+
+
+if __name__ == '__main__':
+    StandaloneApplication(create_app()).run()
