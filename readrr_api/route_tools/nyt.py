ab41fe17919b4890c4c1ac5b492618638e621d52
Michael Rowland <mrowland13@gmail.com>
2020-05-16 09:13:28 -0700

    add get functions

--- readrr_api/route_tools/nyt.py
+++ readrr_api/route_tools/nyt.py
@@ -8,18 +8,19 @@ from requests import get
 class NYT:
 
     def __init__(self):
-        super().__init__()
+        self.NYT_KEY = config('NYT_KEY')
 
-    def all_lists(self):
-        # https://api.nytimes.com/svc/books/v3/lists/names.json?api-key=
-        return
+    def get_lists(self):
+        request_url = f'https://api.nytimes.com/svc/books/v3/lists/names.json?'\
+            f'api-key={self.NYT_KEY}'
+        request = get(request_url)
+        return loads(request.text)
 
     def get_books(self, list_type):
-        NYT_KEY = config('NYT_KEY')
-        request_url = f'https://api.nytimes.com/svc/books/v3/lists/current/{list_type}.json?api-key={NYT_KEY}'
-        results = get(request_url)
-        # WE MAY NEED TO PARSE DATA HERE
-        return loads(results.text)
+        request_url = f'https://api.nytimes.com/svc/books/v3/lists/current/'\
+            f'{list_type}.json?api-key={self.NYT_KEY}'
+        request = get(request_url)
+        return loads(request.text)
     
     def update(self):
         # MAY WANT TO IMPORT FROM ELSEWHERE
