6902ad3ae935189e1636100bf7a47127d07e7fff
jose-marquez89 <jlmarquez89@gmail.com>
2020-05-18 13:27:50 -0500

    adds multithreading to recommendations route

--- readrr_api/routes/recommendations.py
+++ readrr_api/routes/recommendations.py
@@ -3,50 +3,33 @@ import json
 import random
 import pickle
 import logging
+import threading
 
 import requests
 from flask import Flask, request, jsonify, Blueprint
 from sklearn.neighbors import NearestNeighbors
-from ..route_tools.gb_funcs import retrieve_details
-from ..route_tools.gb_search import GBWrapper
+from .. route_tools.gb_funcs import retrieve_details
+from .. route_tools.recommender import Book, tokenize
 # may need cross origin resource sharing (CORS)
 
-FORMAT = "%(asctime)s - %(message)s"
+FORMAT = "%(levelname)s - %(asctime)s - %(message)s"
 logging.basicConfig(level=logging.DEBUG, format=FORMAT)
 # logging.disable(logging.CRITICAL)
 
 recommendations = Blueprint("recommendations", __name__)
 
-file_path = os.path.join(os.path.dirname(__file__),
-                         '..', '..', 'notebooks')
+def fetch_recs(output_list, target_book):
+    """
+    Gets recommendations from the Book class
+    and appends the list that will be part of
+    the response body.
 
-# instantiate api from wrapper
-api = GBWrapper()
-
-# load model dependencies
-with open(os.path.join(file_path, 'knn_model.pkl'), 'rb') as model:
-    knn = pickle.load(model)
-
-with open(os.path.join(file_path, 'compressed_matrix.pkl'), 'rb') as matrix:
-    compressed = pickle.load(matrix)
-
-with open(os.path.join(file_path, 'book_titles.pkl'), 'rb') as books:
-    titles = pickle.load(books)
-
-
-def get_recommendations(book_name, title_reference=titles,
-                        matrix=compressed, model=knn, topn=5):
-    """Returns a list of recommendations based on book title"""
-    recs = []
-    distances, indices = model.kneighbors(
-        matrix[titles.index(book_name)].reshape(1, -1),
-        n_neighbors=topn+1
-    )
-    for neighbor in indices[0]:
-        title = title_reference[neighbor]
-        recs.append(title)
-
-    return recs
+    output_list: list of books to send in response
+    target_book: book in user bookshelf
+    """
+    book = Book(target_book)
+    recs = book.recommendations()
+    output_list.append(recs)
 
 
 @recommendations.route('/recommendations', methods=['POST'])
@@ -55,71 +38,20 @@ def recommend():
     Provide recommendations based on user bookshelf.
     """
     user_books = request.get_json()
-    acquired = False
 
-    # select a random favorite book from which to recommend books
-    favorites = []
+    output = []
+    thread_list = []
 
-    def shelf_iterate(shelf):
-        """Checks shelf for a valid book title"""
-        acquired = False
-        for i in range(len(shelf)):
-            try:
-                target_book = shelf[i]['title']
-                neighbors = get_recommendations(target_book)
-                acquired = True
-                break
-            except ValueError:
-                neighbors = None
-                continue
+    for b in user_books:
+        # start a thread for each book to get recs
+        thread_obj = threading.Thread(target=fetch_recs,
+                                      args=(output, b))
 
-        return neighbors, acquired, target_book
+        thread_list.append(thread_obj)
+        thread_object.start()
 
-    for book in user_books:
-        if book['favorite']:
-            favorites.append(book['title'])
-
-    # to add some randomness
-    random.shuffle(favorites)
-
-    # if there are no favorites, recommend based on first book
-    if len(favorites) >= 1:
-        for i in range(len(favorites)):
-            try:
-                target_book = favorites[i]
-                neighbors = get_recommendations(target_book)
-                acquired = True
-                break
-            except ValueError:
-                continue
-
-    else:
-        neighbors, acquired, target_book = shelf_iterate(user_books)
-
-    # if book is not in known titles, recommend an alternative
-    if not acquired:
-        neighbors, acquired, target_book = shelf_iterate(user_books)
-        if neighbors is None:
-            target_book = "#GIRLBOSS"
-            neighbors = get_recommendations(target_book)
-    else:
-        neighbors = get_recommendations(target_book)
-
-    output_recs = []
-    logging.info("Neighbors:" + str(neighbors))
-
-    # skip first book
-    for book in neighbors[1:]:
-        book_data = api.search(book)
-        target_data = book_data['items'][0]
-        target_json = retrieve_details(target_data)
-        output_recs.append(target_json)
-
-    # change 'id' to 'googleId' at every list index
-    for item in output_recs:
-        item['googleId'] = item.pop('id')
-
-    output = {'based_on': target_book,
-              'recommendations': output_recs}
+    # wait for threads to end
+    for thread in threads:
+        thread.join()
 
     return jsonify(output)
